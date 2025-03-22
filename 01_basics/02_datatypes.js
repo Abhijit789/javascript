279d74aceb23b20e462d3067be038ea47e01020f
Abhijit789 <107102144+Abhijit789@users.noreply.github.com>
2025-03-22 16:05:08 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,35 @@
+"use strict";
+console.log("hello"); console.log("bye");
+
+let name="hello";
+console.log(typeof name);
+name=20;
+console.log(name);
+
+
+let num=1234;
+console.log(typeof num);
+
+let bool=true;
+console.log(typeof bool);
+bool=20;
+console.log(bool);
+
+
+let un;
+console.log(typeof un);
+
+let obj={
+    name:"ajay",
+    age:20,
+    function:()=>{
+        console.log("hello function");
+        
+    }
+}
+
+console.log(typeof obj.function);
+obj.function()
+
+
+
---
+++ 01_basics/test.js
@@ -0,0 +1,30 @@
+// console.log("hello");
+// let nam="abhijit";
+// nam="ajay";
+// console.log(nam);
+
+// // age=23;
+// // let age;
+// // console.log(age);
+// console.log(hoisting);
+
+// hoisting="hoist";
+// var hoisting;
+// console.log(hoisting);
+
+// let initialization;
+// console.log("initialization",initialization);
+
+// const initialize=10;
+// initialize=20;//we cant reinitiliza the const var
+// console.log("const reinitialize",initialize);
+
+
+const accondId="1234";
+let accounName="Ajay";
+var accoundBal="123";
+
+// var state;
+console.table([accondId,accounName,accoundBal]);
+
+// dont prefer var 
