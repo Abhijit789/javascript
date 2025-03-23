37e01e3af69e535da766db5ab0ce2fafd65ca92c
Abhijit789 <107102144+Abhijit789@users.noreply.github.com>
2025-03-23 12:45:18 +0000

    typeconversion

---
+++ 01_basics/03_typeconversion.js
@@ -0,0 +1,37 @@
+let type=false;
+
+let valueConversion=Number(type)
+console.log(typeof valueConversion);
+
+let add=true-"bye";
+console.log(add);
+
+let item=true+false;
+console.log(item);
+
+let item2=null+undefined;
+console.log(item2);
+
+let item3=null+null;
+console.log(item3);
+
+let item4=null+true;
+console.log(item4);
+
+let item5=true+undefined;
+console.log(item5);
+
+let item6="33"+"33"-"33"
+console.log(item6);
+
+let item7=33;
+
+let isLogdIn=Boolean(item7)
+console.log(typeof isLogdIn);
+console.log(isLogdIn);
+
+
+
+
+
+
